79a38b61923387ea1998b2c8799fc3f442f344cc
Brandon Reyes <brandon.reyesmi@gmail.com>
2020-09-11 11:44:33 -0500

    Docs agregado

---
+++ docs/precache-manifest.07d3e2f206d9973b1557e19da9fd20ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a199de387cc3fe387714d442f296584",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6fc85b7e497353a6d406",
+    "url": "/static/css/main.92f5345e.chunk.css"
+  },
+  {
+    "revision": "9948515d998a14a1480d",
+    "url": "/static/js/2.6a91807f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6a91807f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fc85b7e497353a6d406",
+    "url": "/static/js/main.3b83d5ac.chunk.js"
+  },
+  {
+    "revision": "9d3929294d677b1b9ff6",
+    "url": "/static/js/runtime-main.aa9df6a3.js"
+  }
+]);
